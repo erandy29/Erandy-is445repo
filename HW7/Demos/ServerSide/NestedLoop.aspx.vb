8085cfc9307e6aabd37c1016a1b34db163b7f371
erandy29 <erandyfuentes@yahoo.com>
2013-05-13 21:28:23 -0700

    css

--- HW7/Demos/ServerSide/NestedLoop.aspx.vb
+++ HW7/Demos/ServerSide/NestedLoop.aspx.vb
@@ -6,6 +6,48 @@
     End Sub
 
     Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles NestedCalculateButton.Click
+        If (NestedTextBox >2 && NestedTextBox <=10){
+             document.write('<h2> Your input number is ' + numberinput + '</h2>');
+    var dividedby2 = numberinput;
+    var count = 0;
+    while (dividedby2 >= 0.000001) {
+        dividedby2 = dividedby2 / 2;
+        count++;
+    }
+
+
+
+                Else
+
+                 {
+    document.write('<h2 id="incorrect" />Your input is ' + numberinput + '. The valid input number is between 2 and 10. Please reload this page and try again. </h2>');
+
+}
+
+
+
+
+        End If
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     End Sub
 End Class
